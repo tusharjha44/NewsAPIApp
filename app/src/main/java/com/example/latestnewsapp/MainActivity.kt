e5854597a681270a2d5b793ea321b4fd52e2a3b8
tusharjha44 <85125680+tusharjha44@users.noreply.github.com>
2022-06-21 21:36:16 +0530

    COmpleted UI and Navigation between fragments.

--- app/src/main/java/com/example/latestnewsapp/MainActivity.kt
+++ app/src/main/java/com/example/latestnewsapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.latestnewsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.latestnewsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bnvNews.setupWithNavController(navController)
+
     }
 }
--- app/src/main/java/com/example/latestnewsapp/presentation/di/RemoteDataSourceModule.kt
+++ app/src/main/java/com/example/latestnewsapp/presentation/di/RemoteDataSourceModule.kt
@@ -3,9 +3,14 @@ package com.example.latestnewsapp.presentation.di
 import com.example.latestnewsapp.data.api.NewsService
 import com.example.latestnewsapp.data.repository.dataSource.NewsRemoteDataSource
 import com.example.latestnewsapp.data.repository.dataSourceImpl.NewsRemoteDataSourceImpl
+import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
+@Module
+@InstallIn(SingletonComponent::class)
 class RemoteDataSourceModule {
     @Singleton
     @Provides
